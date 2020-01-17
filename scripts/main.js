3b3b9d3b56b29b633fd76dba6cffd3e5ed63d35b
Anushka Verma <v.anushka786@gmail.com>
2020-01-17 23:33:49 +0530

    add name block

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+(function ($) {
+	"use strict";
+
+	$('#text-slider').owlCarousel({
+		nav: false,
+		items: 1,
+		navSpeed: 400,
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 4000,
+		autoplayHoverPause: true,
+	});
+
+
+
+})(jQuery);
